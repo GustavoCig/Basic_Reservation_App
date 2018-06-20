62f7b262c1b7c6caaf751b2d93fdee6550db5f32
GustavoCig <gustavocignachi@hotmail.com>
2018-06-20 09:41:26 -0300

    Initial commit
    Initial commit for jQuery Modal Confirmation and Modal Forms example.
    Meant as a test/example of a functionality to be added in other projects.
    
    Commit:Add
    
    TaskNumber:-

---
+++ script.js
@@ -0,0 +1,90 @@
+$( function() {
+	    var alertWindow, reservationDialog, reservationForm,
+	 
+	      // From http://www.whatwg.org/specs/web-apps/current-work/multipage/states-of-the-type-attribute.html#e-mail-state-%28type=email%29
+	      time = $( "#time" ),
+	      description = $( "#description" ),
+	      allFields = $( [] ).add( time ).add( description ),
+	      tips = $( ".validateTips" );
+	 
+	    function updateTips( t ) {
+	      tips
+	        .text( t )
+	        .addClass( "ui-state-highlight" );
+	      setTimeout(function() {
+	        tips.removeClass( "ui-state-highlight", 1500 );
+	      }, 500 );
+	    }
+	 
+	    function checkLength( o, n, min, max ) {
+	      if ( o.val().length > max || o.val().length < min ) {
+	        o.addClass( "ui-state-error" );
+	        updateTips( "Quantidade de caracteres da " + n + " deve estar entre " +
+	          min + " e " + max + "." );
+	        return false;
+	      } else {
+	        return true;
+	      }
+	    }
+	 
+	    function finishReservation() {
+	      var valid = true;
+	      allFields.removeClass( "ui-state-error" );
+	 
+	      valid = valid && checkLength( time, "username", 3, 16 );
+	      valid = valid && checkLength( description, "descrição", 1, 585 );
+	 
+	      if ( valid ) {
+	        $( "#users tbody" ).append( "<tr>" +
+	          "<td>" + time.val() + "</td>" +
+	          "<td>" + description.val() + "</td>" +
+	        "</tr>" );
+	        reservationDialog.dialog( "close" );
+	      }
+	      return valid;
+	    }
+
+	    alertWindow = $( "#dialog-confirm" ).dialog({
+	      autoOpen: false,
+	      resizable: false,
+	      height: "auto",
+	      width: 400,
+	      modal: true,
+	      buttons: {
+	        "Continuar": function() {
+	          $( this ).dialog( "close" );
+	          reservationDialog.dialog( "open" );
+	        },
+	        Cancelar: function() {
+	          $( this ).dialog( "close" );
+	        }
+	      }
+	    });
+	 
+	    reservationDialog = $( "#dialog-form" ).dialog({
+	      autoOpen: false,
+	      resizable: false,
+	      height: "auto",
+	      width: 700,
+	      modal: true,
+	      buttons: {
+	        "Enviar requisição": finishReservation,
+	        Cancelar: function() {
+	          	reservationDialog.dialog( "close" );
+	        }
+	      },
+	      close: function() {
+	        form[ 0 ].reset();
+	        allFields.removeClass( "ui-state-error" );
+	      }
+	    });
+	 
+	    form = reservationDialog.find( "form" ).on( "submit", function( event ) {
+	      event.preventDefault();
+	      finishReservation();
+	    });
+	 
+	    $( "#special-cases" ).button().on( "click", function() {
+	      alertWindow.dialog( "open" );
+	    });
+	  } );
